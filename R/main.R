076a6669a3100218d671585f114ec9b1382f16f6
Ashby Thorpe <ashbythorpe@gmail.com>
2022-12-26 17:41:39 +0000

    Improved CLI

--- R/main.R
+++ R/main.R
@@ -1,6 +1,5 @@
 perform_tasks <- function(tasks = dr_frost_tasks(), email = NULL, 
-                          password = NULL, keyring = NULL, end = TRUE,
-                          safely = TRUE) {
+                          password = NULL, keyring = NULL, end = TRUE) {
   tasks <- get_tasks(tasks)
   if(is.null(tasks)) {
     cli::cli_alert_info("No tasks specified.")
@@ -11,7 +10,7 @@ perform_tasks <- function(tasks = dr_frost_tasks(), email = NULL,
   
   cli::cli_alert_info("Beginning tasks.")
   
-  results <- lapply(tasks, perform_task, safely = safely)
+  results <- lapply(tasks, perform_task)
   results_df <- vctrs::vec_rbind(result_init,!!!results)
   
   if(!any(results_df$completed)) {
@@ -23,30 +22,18 @@ perform_tasks <- function(tasks = dr_frost_tasks(), email = NULL,
   }
   
   if(end) {
-    tdf$driver_utils$end_session()
+    try(tdf$driver_utils$end_session())
   }
   
   invisible(results_df)
 }
 
-perform_task <- function(task, safely) {
+perform_task <- function(task) {
   cli::cli_alert_info("Task: {.val {task}}.")
   points <- NA
   error <- NA
-  if(safely) {
-    error <- try({
-      switch(
-        task,
-        addition_subtraction = tdf$KS3$Number$addition_subtraction(),
-        multiplication = tdf$KS3$Number$multiplication(),
-        pictoral_division = tdf$KS3$Number$pictoral_division(),
-        division = tdf$KS3$Number$division()
-      )
-      points <- tdf$driver_utils$get_points()
-      cli::cli_alert_success("Task completed.")
-      cli::cli_alert_success("Points obtained: {.val {points}}.")
-    }, silent = TRUE)
-  } else {
+  
+  rlang::try_fetch({
     switch(
       task,
       addition_subtraction = tdf$KS3$Number$addition_subtraction(),
@@ -54,13 +41,23 @@ perform_task <- function(task, safely) {
       pictoral_division = tdf$KS3$Number$pictoral_division(),
       division = tdf$KS3$Number$division()
     )
-    points <- tdf$driver_utils$get_points()
     cli::cli_alert_success("Task completed.")
-    cli::cli_alert_success("Points obtained: {.val {points}}.")
+  }, error = function(c) {
+    error <- c
+    cli::cli_alert_danger("Task failed.")
+  })
+  
+  if(is.na(error)) {
+    rlang::try_fetch({
+      points <- tdf$driver_utils$get_points()
+      cli::cli_alert_success("Points obtained: {.val {points}}.")
+    }, error = function(c) {
+      error <- c
+      cli::cli_alert_danger("Could not get points.")
+    })
   }
   
   if(is.na(points)) {
-    cli::cli_alert_danger("Task failed.")
     res <- data.frame(
       task = task,
       completed = FALSE,
